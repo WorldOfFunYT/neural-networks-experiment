5d69ed610926d7126f624c678fa788db2e1e71f0
WorldOfFunYT <czarczynskiszymon@gmail.com>
2023-05-20 12:23:51 +0100

    Sensor rays (no collision detection yet)

--- car.js
+++ car.js
@@ -11,10 +11,12 @@ class Car {
         this.friction = 0.05;
         this.angle = 0;
 
+        this.sensor = new Sensor(this);
         this.controls = new Controls();
     }
-    update() {
+    update(roadBorders) {
         this.#move();
+        this.sensor.update(roadBorders);
     }
     #move() {
         // Move controls
@@ -60,5 +62,6 @@ class Car {
         ctx.rect(-this.width / 2, -this.height / 2, this.width, this.height);
         ctx.fill();
         ctx.restore();
+        this.sensor.draw(ctx);
     }
 }
